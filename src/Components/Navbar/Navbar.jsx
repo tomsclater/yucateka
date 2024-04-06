d889b9ad57879b67f8bf01c09540610f38d81e61
Tsclater <tomsclater@live.com>
2024-04-05 20:17:06 -0700

    20:28

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="container">
         <img src={logo} alt="" className="logo" />
         <ul>
             <li>Home</li>
