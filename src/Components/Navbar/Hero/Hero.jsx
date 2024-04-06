998ae3dfe20e12725da36d36d0890e5f8e56d229
Tsclater <tomsclater@live.com>
2024-04-06 01:43:20 -0700

    28:31

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -3,11 +3,18 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero container">
         <div className="hero-text">
             <h1>
             Uncovering art's beauty, one masterpiece at a time.
             </h1>
+            <p>
+            Yucateka Gallery stands as a vibrant hub for artistic expression, where every stroke of paint, every sculpted form, and every captured moment holds the power to captivate.
+            Find your connection to the world of art and let it transform your perspective. Explore our diverse collection, attend exhibitions or classes, and immerse yourself in creativity.
+            </p>
+            <button className="btn">
+                Explore more
+            </button>
         </div>
     </div>
   )
