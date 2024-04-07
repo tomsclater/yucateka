fab0203e3429724a58d1b862a1a27ebcf972eda3
Tsclater <tomsclater@live.com>
2024-04-06 17:34:32 -0700

    43:45

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -14,21 +14,21 @@ const Programs = () => {
         <div className="program">
             <img src={program_1} alt="" />
             <div className="caption">
-                <img src={program_icon_1} alt="" />
+                <img className="invert" src={program_icon_1} alt="" />
                 <p>Exhibition Spaces</p>
             </div>
         </div>
         <div className="program">
             <img src={program_2} alt="" />
             <div className="caption">
-                <img src={program_icon_2} alt="" />
+                <img className="invert" src={program_icon_2} alt="" />
                 <p>Art Collections</p>
             </div>
         </div>
         <div className="program">
             <img src={program_3} alt="" />
             <div className="caption">
-                <img src={program_icon_3} alt="" />
+                <img className="invert"  src={program_icon_3} alt="" />
                 <p>Events & Workshops</p>
             </div>
         </div>
