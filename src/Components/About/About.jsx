2a9a989d320c3969bb81947d493c664de6f4057e
Tsclater <tomsclater@live.com>
2024-04-07 09:40:31 -0700

    55:24

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './About.css'
 import about_img from '../../assets/about.jpg'
-// import play_icon from '../../assets/play-icon.png'
+import play_icon from '../../assets/play-icon.png'
 
 const About = () => {
   return (
     <div className="about">
         <div className="about-left">
             <img src={about_img} alt="" className="about-img" />
+            <img src={play_icon} alt="" className="play-icon" />
+        </div>
+        <div className="about-right">
+            <h3>About Gallery and Classes</h3>
+            <h2>Celebrating Artistic Expression</h2>
+            <p>Embark on an enriching journey through the captivating world of art with our gallery's vibrant offerings. Our diverse collection of artworks spans across genres, mediums, and cultures, providing a dynamic platform for artists and art enthusiasts alike.
+            </p>
+            With a commitment to showcasing creativity, fostering dialogue, and nurturing talent, our gallery serves as a nurturing hub for artistic expression and exploration. From solo exhibitions showcasing emerging talents to group shows celebrating established artists, we offer a curated experience that inspires and delights.
+            <p>
+            Whether you're a seasoned collector, a budding artist, or simply an admirer of beauty, our gallery invites you to immerse yourself in the transformative power of art and discover new perspectives that enrich the soul.
+            </p>
         </div>
     </div>
   )
