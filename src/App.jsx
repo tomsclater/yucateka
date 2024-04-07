f4bdf9b46d813ddd6311135bef09f7d7d28a1181
Tsclater <tomsclater@live.com>
2024-04-06 18:13:02 -0700

    51:58

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <div className="container">
-          <Title/>
+          <Title subTitle='Our Gallery' title='What We Offer'/>
           <Programs/>
         </div>
     </div>
