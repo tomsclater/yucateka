c7943671468d522ba589bdaaad8028bba7945d01
Tsclater <tomsclater@live.com>
2024-04-08 01:23:59 -0700

    Update

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './Testimonials.css'
 import next_icon from '../../assets/next-icon.png'
 import back_icon from '../../assets/back-icon.png'
-import user_1 from '../../assets/user-1.png'
-import user_2 from '../../assets/user-2.png'
-import user_3 from '../../assets/user-3.png'
-import user_4 from '../../assets/user-4.png'
+import user_1 from '../../assets/user-1.jpg'
+import user_2 from '../../assets/user-2.jpg'
+import user_3 from '../../assets/user-3.jpg'
+import user_4 from '../../assets/user-4.jpg'
 
 
 const Testimonials = () => {
