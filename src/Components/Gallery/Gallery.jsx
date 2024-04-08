253337d5708bdcafaa7ce053eefd4d8dc8822c6f
Tsclater <tomsclater@live.com>
2024-04-07 21:56:28 -0700

    1:18:47

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -13,8 +13,10 @@ const Gallery = () => {
             <img src={gallery_2} alt="" />
             <img src={gallery_3} alt="" />
             <img src={gallery_4} alt="" />
-
         </div>
+        <button className="btn dark-btn">
+            See more here 🡢
+        </button>
     </div>
   )
 }
