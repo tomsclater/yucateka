0c5846ccea3c83e967b291656a52c6820bbb71da
Tsclater <tomsclater@live.com>
2024-04-13 19:26:08 -0700

    1:48:24

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className="contact">
+        <div className="contact-col">
+            <h3>Send us a message</h3>
+            <p>Feel free to drop us a message to get more information below.
+            </p>
+            <ul>
+                <li>yucatekart@gmail.com</li>
+                <li>+1 123-345-456</li>
+                <li>123 Mexico Lane, Mexico</li>
+            </ul>
+        </div>
+        <div className="contact-col"></div>
+    </div>
+  )
+}
+
+export default Contact
