748cca9140b8a35d49c56c16c4bfba442e92c991
Tsclater <tomsclater@live.com>
2024-04-13 23:17:44 -0700

    1:55:16

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
 
 const Contact = () => {
   return (
     <div className="contact">
         <div className="contact-col">
-            <h3>Send us a message</h3>
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
             <p>Feel free to drop us a message to get more information below.
             </p>
             <ul>
-                <li>yucatekart@gmail.com</li>
-                <li>+1 123-345-456</li>
-                <li>123 Mexico Lane, Mexico</li>
+                <li><img src={mail_icon} alt="" />yucatekart@gmail.com</li>
+                <li><img src={phone_icon} alt="" />+1 123-345-456</li>
+                <li><img src={location_icon} alt="" />123 Mexico Lane, Mexico</li>
             </ul>
         </div>
         <div className="contact-col"></div>
