971487387e56c0528f2e9b2bdff806639cba52fd
Tsclater <tomsclater@live.com>
2024-04-25 20:32:16 -0700

    2:12:09

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,14 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <p>Copyright 2024 Yucateka. All rights reserved.</p>
+      <ul>
+        <li>Terms of Services</li>
+        <li>Privacy Policy</li>
+        
+      </ul>
+    </div>
   )
 }
 
