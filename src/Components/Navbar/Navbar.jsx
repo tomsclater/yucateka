be0155dbfa6173afb754eae4798167eaed93bbc8
Tsclater <tomsclater@live.com>
2024-04-25 22:17:31 -0700

    2:18:37

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -17,12 +18,12 @@ const Navbar = () => {
     <nav className={`'container ${sticky? 'dark-nav' : ''}`}>
         <img src={logo} alt="" className="logo" />
         <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Gallery</li>
-            <li>Events</li>
-            <li>Testimonials</li>
-            <li><button className="btn">Contact us</button></li>
+            <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><Link to='programs' smooth={true} offset={-260} duration={500}>About Us</Link></li>
+            <li><Link to='gallery' smooth={true} offset={-260} duration={500}>Gallery</Link></li>
+            <li><Link to='events' smooth={true} offset={-260} duration={500}>Events</Link></li>
+            <li><Link to='tesimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500} className="btn">Contact us</Link></li>
         </ul>
     </nav>
   )
